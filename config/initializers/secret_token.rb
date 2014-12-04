209f75623e8fb2c77ae29a7bc8c55d20ead8f33e
Soule Designs <soule.designs@gmail.com>
2014-12-03 20:59:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laurasoule::Application.config.secret_token = 'f38a2a70395c5f68ff8e42741c9f2fd3f75af3be1785e806b5b3d4037166382e213778fd6705b205830d61adb446eee82b8d4ce4ee6e2722c8946287d36a643c'
